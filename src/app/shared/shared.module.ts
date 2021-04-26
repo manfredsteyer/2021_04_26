d72ac5282839e4e667a561af967278ffa3819a4d
Manfred Steyer <manfred.steyer@gmx.net>
2021-04-26 10:00:44 +0200

    tabbed-pane: v1

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,50 @@
+// src/app/shared/shared.module.ts
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DateComponent } from './date/date.component';
+import { CityPipe } from './city.pipe';
+import { StatusColorPipe } from './status-color.pipe';
+import { StatusFilterPipe } from './status-filter.pipe';
+import { FormsModule } from '@angular/forms';
+
+// Von der CLI eingefügt
+import { CityValidationDirective } from './validation/city-validation.directive';
+import { TabbedPaneComponent } from './tabs/tabbed-pane/tabbed-pane.component';
+import { TabComponent } from './tabs/tab/tab.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+  ],
+  declarations: [
+    DateComponent,
+    CityPipe,
+    StatusColorPipe,
+    StatusFilterPipe,
+
+    // Von der CLI eingefügt
+    CityValidationDirective,
+
+    TabbedPaneComponent,
+
+    TabComponent
+  ],
+  exports: [
+    DateComponent,
+    CityPipe,
+    StatusColorPipe,
+    StatusFilterPipe,
+    FormsModule,
+    CommonModule,
+
+    // Neue Einträge
+    CityValidationDirective,
+
+    TabbedPaneComponent,
+
+    TabComponent
+  ]
+})
+export class SharedModule { }
---
+++ src/app/shared/tabs/tab/tab.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { TabbedPaneComponent } from '../tabbed-pane/tabbed-pane.component';
+
+@Component({
+  selector: 'app-tab',
+  templateUrl: './tab.component.html',
+  styleUrls: ['./tab.component.scss']
+})
+export class TabComponent implements OnInit {
+
+  @Input() title = '';
+  visible = true;
+
+  constructor(private pane: TabbedPaneComponent) {
+  }
+
+  ngOnInit(): void {
+    this.pane.register(this);
+  }
+
+}
